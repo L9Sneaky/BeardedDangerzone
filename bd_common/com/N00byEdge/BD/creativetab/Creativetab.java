c9256c404afef37c7e64dbece7848287ad24aa98
Hannes Bredberg <hannesbredberg@gmail.com>
2013-05-02 19:02:22 +0200

    HurrDurr

--- bd_common/com/N00byEdge/BD/creativetab/Creativetab.java
+++ bd_common/com/N00byEdge/BD/creativetab/Creativetab.java
@@ -2,13 +2,20 @@ package com.N00byEdge.BD.creativetab;
 
 import com.N00byEdge.BD.lib.Ids;
 
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
 import net.minecraft.creativetab.*;
 import net.minecraft.item.ItemStack;
 
 public class Creativetab {
-    public static CreativeTabs tabBD = new CreativeTabs("tabCustom") {
+    public static CreativeTabs tabBD = new CreativeTabs("tabBD") {
         public ItemStack getIconItemStack() {
             return new ItemStack(Ids.RawBeardID, 1, 0);
         }
     };
+
+    public static void InitializeCreativeTabs() {
+        LanguageRegistry.instance().addStringLocalization(
+                "itemGroup.tabBD", "en_US", "Bearded Dangerzone");
+    }
 }
