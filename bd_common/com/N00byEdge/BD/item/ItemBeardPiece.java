62bb501cba22b86ecf891774c8a730351e4a73d7
Hannes Bredberg <hannesbredberg@gmail.com>
2013-05-08 20:19:19 +0200

    1.5.2 update

--- bd_common/com/N00byEdge/BD/item/ItemBeardPiece.java
+++ bd_common/com/N00byEdge/BD/item/ItemBeardPiece.java
@@ -1,18 +1,11 @@
 package com.N00byEdge.BD.item;
 
-import com.N00byEdge.BD.creativetab.*;
-
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
+import com.N00byEdge.BD.creativetab.Creativetab;
 
 public class ItemBeardPiece extends Item {
     public ItemBeardPiece(int id) {
         super(id);
         this.setCreativeTab(Creativetab.tabBD);
-    }
-
-    @Override
-    public void updateIcons(IconRegister iconReg) {
-        iconIndex = iconReg.registerIcon(this.getUnlocalizedName());
     }
 }
