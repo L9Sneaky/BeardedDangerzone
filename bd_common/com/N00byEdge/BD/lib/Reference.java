a43cf64ad6124b1cc1c8501d5c2aa9d260f80099
UltimateMCExplorer <UltimateMCExplorer@users.noreply.github.com>
2013-12-06 22:21:12 +0000

    Update Reference.java
    0.1e Fix

--- bd_common/com/N00byEdge/BD/lib/Reference.java
+++ bd_common/com/N00byEdge/BD/lib/Reference.java
@@ -3,6 +3,6 @@ package com.N00byEdge.BD.lib;
 public class Reference {
     public static final String MODID = "n00byedge_bd";
     public static final String MODNAME = "Bearded Dangerzone";
-    public static final String VERSION = "0.1d";
+    public static final String VERSION = "0.1e";
 	public static boolean IC2INTEGRATION;
 }
